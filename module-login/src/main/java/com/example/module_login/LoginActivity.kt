b335bbee0211bb7c4452bdb6cd7d25f56edbf910
apple <18201497620@163.com>
2020-08-09 23:54:31 +0800

    登录页面细节优化

--- module-login/src/main/java/com/example/module_login/LoginActivity.kt
+++ module-login/src/main/java/com/example/module_login/LoginActivity.kt
@@ -4,6 +4,7 @@ import com.alibaba.android.arouter.facade.annotation.Route
 import com.example.base.network.base.activity.BaseMvvmActivity
 import com.example.base.network.route.RoutePath
 import com.example.module_login.databinding.ActivityLoginBinding
+import kotlinx.android.synthetic.main.activity_login.*
 
 @Route(path = RoutePath.Login.LOGIN_ACTIVITY)
 class LoginActivity : BaseMvvmActivity<ActivityLoginBinding, LoginViewModel>() {
@@ -13,4 +14,8 @@ class LoginActivity : BaseMvvmActivity<ActivityLoginBinding, LoginViewModel>() {
     override fun statusBarColor(): Int = R.color.black
 
     override fun isDarkFont(): Boolean = false
+
+    override fun init() {
+        ivClose.setOnClickListener { finish() }
+    }
 }
