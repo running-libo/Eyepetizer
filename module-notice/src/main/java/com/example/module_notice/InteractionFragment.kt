4f996eea2b1f52bd8d3a8ac2d93c4646df6a3ded
apple <18201497620@163.com>
2020-08-23 21:51:29 +0800

    通知提示页面完成

--- module-notice/src/main/java/com/example/module_notice/InteractionFragment.kt
+++ module-notice/src/main/java/com/example/module_notice/InteractionFragment.kt
@@ -3,11 +3,17 @@ package com.example.module_notice
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.alibaba.android.arouter.launcher.ARouter
 import com.example.base.network.base.fragment.BaseLazyloadFragment
+import com.example.base.network.route.RoutePath
+import kotlinx.android.synthetic.main.fragment_interaction.*
 
 class InteractionFragment : BaseLazyloadFragment() {
-    override fun initView() {
 
+    override fun initView() {
+        tvLogin.setOnClickListener {
+            ARouter.getInstance().build(RoutePath.Login.LOGIN_ACTIVITY).navigation()
+        }
     }
 
     override fun getContentView(inflater: LayoutInflater, container: ViewGroup?): View {
