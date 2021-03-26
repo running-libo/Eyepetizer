2708b7a779061e6e8026e3bcbc428737eb4e7dcc
apple <18201497620@163.com>
2021-03-26 11:12:39 +0800

    项目组件全部改为远程依赖，只留下空壳app模块

--- module-home/src/main/java/com/example/module_home/module/home/FindFragment.kt
+++ module-home/src/main/java/com/example/module_home/module/home/FindFragment.kt
@@ -3,7 +3,6 @@ package com.example.module_home.module.home
 import com.example.base.network.base.fragment.BaseMvvmFragment
 import com.example.module_home.databinding.FragmentFindBinding
 import com.example.module_home.viewmodel.FindViewModel
-import com.example.projectlib.DateUtils
 
 class FindFragment : BaseMvvmFragment<FragmentFindBinding, FindViewModel>() {
 
